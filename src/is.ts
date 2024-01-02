ef16722f9cffcd4ae28ce8b0433a8994aa2c8d97
vino <vino0908@outlook.com>
2024-01-02 23:06:25 +0800

    add is function
    add is function

--- src/is.ts
+++ src/is.ts
@@ -15,3 +15,23 @@ export function isNumber(val: unknown): boolean {
 export function isString(val: unknown): boolean {
   return typeof val === 'string'
 }
+
+export function isBoolean(val: unknown): boolean {
+  return typeof val === 'boolean'
+}
+
+export function isObject(val: unknown): boolean {
+  return typeof val === 'object' && val !== null
+}
+
+export function isArray(val: unknown): boolean {
+  return Array.isArray(val)
+}
+
+export function isFunction(val: unknown): boolean {
+  return typeof val === 'function'
+}
+
+export function isNull(val: unknown): boolean {
+  return val === null
+}
--- test/is.test.ts
+++ test/is.test.ts
@@ -1,5 +1,6 @@
 import { it } from 'vitest'
-import { isEmail, isNumber, isString, isUrl } from '../src'
+import { isArray, isBoolean, isEmail, isFunction, isNumber, isObject, isString, isUrl } from '../src'
+import { isArray, isBoolean, isEmail, isFunction, isNull, isNumber, isObject, isString, isUrl } from '../src'
 
 it('isEmail', (t) => {
   t.expect(isEmail('vino')).toBe(false)
@@ -49,4 +50,84 @@ it('isString', (t) => {
   t.expect(isString(BigInt('111'))).toBe(false)
   t.expect(isString(null)).toBe(false)
   t.expect(isString(undefined)).toBe(false)
+})
+
+it('isBoolean', (t) => {
+  t.expect(isBoolean(true)).toBe(true)
+  t.expect(isBoolean(false)).toBe(true)
+  t.expect(isBoolean('0')).toBe(false)
+  t.expect(isBoolean(0)).toBe(false)
+  t.expect(isBoolean(Number.NaN)).toBe(false)
+  t.expect(isBoolean({})).toBe(false)
+  t.expect(isBoolean(BigInt('111'))).toBe(false)
+  t.expect(isBoolean(null)).toBe(false)
+  t.expect(isBoolean(undefined)).toBe(false)
+})
+
+it('isObject', (t) => {
+  t.expect(isObject({})).toBe(true)
+  t.expect(isObject([])).toBe(true)
+  t.expect(isObject(new Date())).toBe(true)
+  t.expect(isObject(/[a-z]/)).toBe(true)
+  t.expect(isObject(false)).toBe(false)
+  t.expect(isObject('0')).toBe(false)
+  t.expect(isObject(0)).toBe(false)
+  t.expect(isObject(Number.NaN)).toBe(false)
+  t.expect(isObject(BigInt('111'))).toBe(false)
+  t.expect(isObject(null)).toBe(false)
+  t.expect(isObject(undefined)).toBe(false)
+})
+
+it('isArray', (t) => {
+  t.expect(isArray([])).toBe(true)
+  t.expect(isArray({})).toBe(false)
+  t.expect(isArray(new Date())).toBe(false)
+  t.expect(isArray(/[a-z]/)).toBe(false)
+  t.expect(isArray(false)).toBe(false)
+  t.expect(isArray('0')).toBe(false)
+  t.expect(isArray(0)).toBe(false)
+  t.expect(isArray(Number.NaN)).toBe(false)
+  t.expect(isArray(BigInt('111'))).toBe(false)
+  t.expect(isArray(null)).toBe(false)
+  t.expect(isArray(undefined)).toBe(false)
+})
+
+it('isFunction', (t) => {
+  const testFn = async () => {}
+  const testFn2 = function () {}
+  const testFn3 = () => {}
+  const testFn4 = () => new Promise(resolve => resolve(1))
+  const testFn5 = () => 1
+  t.expect(isFunction(testFn)).toBe(true)
+  t.expect(isFunction(testFn2)).toBe(true)
+  t.expect(isFunction(testFn3)).toBe(true)
+  t.expect(isFunction(testFn4)).toBe(true)
+  t.expect(isFunction(testFn5)).toBe(true)
+
+  t.expect(isFunction([])).toBe(false)
+  t.expect(isFunction({})).toBe(false)
+  t.expect(isFunction(new Date())).toBe(false)
+  t.expect(isFunction(/[a-z]/)).toBe(false)
+  t.expect(isFunction(false)).toBe(false)
+  t.expect(isFunction('0')).toBe(false)
+  t.expect(isFunction(0)).toBe(false)
+  t.expect(isFunction(Number.NaN)).toBe(false)
+  t.expect(isFunction(BigInt('111'))).toBe(false)
+  t.expect(isFunction(null)).toBe(false)
+  t.expect(isFunction(undefined)).toBe(false)
+})
+
+it('isNull', (t) => {
+  t.expect(isNull(null)).toBe(true)
+
+  t.expect(isNull([])).toBe(false)
+  t.expect(isNull({})).toBe(false)
+  t.expect(isNull(new Date())).toBe(false)
+  t.expect(isNull(/[a-z]/)).toBe(false)
+  t.expect(isNull(false)).toBe(false)
+  t.expect(isNull('0')).toBe(false)
+  t.expect(isNull(0)).toBe(false)
+  t.expect(isNull(Number.NaN)).toBe(false)
+  t.expect(isNull(BigInt('111'))).toBe(false)
+  t.expect(isNull(undefined)).toBe(false)
 })
